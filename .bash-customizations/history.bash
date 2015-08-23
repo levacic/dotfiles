760417d7390c71f09f6f09cb8a99760447653e67
Milos Levacic <milos@levacic.net>
2015-08-23 07:31:37 +0200

    Add history configuration to bash customizations

---
+++ .bash-customizations/history.bash
@@ -0,0 +1,10 @@
+# Don't put duplicate lines or lines starting with space in the history. See
+# bash(1) for more options.
+export HISTCONTROL=ignoreboth
+
+# Append to the history file, don't overwrite it
+shopt -s histappend
+
+# For setting history length see HISTSIZE and HISTFILESIZE in bash(1),
+export HISTSIZE=1000
+export HISTFILESIZE=2000
