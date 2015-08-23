7cfb3c13c868c61058ecf7e559461267c4f06afb
Milos Levacic <milos@levacic.net>
2015-08-23 07:50:09 +0200

    Update ZSH Ruby initialization script
    This way, it handles multiple specific Ruby versions I've got installed
    on separate boxes. There is probably a sensible way to make this more
    generic, but no need to bother with that for now.

--- .oh-my-zsh-customizations/rvm.zsh
+++ .oh-my-zsh-customizations/rvm.zsh
@@ -4,9 +4,15 @@ if [[ -s "${HOME}/.rvm/scripts/rvm" ]] ; then
 fi
 
 # Run them Rubies
-PATH="${PATH}:${HOME}/.rvm/gems/ruby-1.9.3-p484/bin"
-PATH="${PATH}:${HOME}/.rvm/gems/ruby-1.9.3-p484@global/bin"
-PATH="${PATH}:${HOME}/.rvm/rubies/ruby-1.9.3-p484/bin"
+if [[ -e "${HOME}/.rvm/rubies/ruby-1.9.3-p484" ]]; then
+    PATH="${PATH}:${HOME}/.rvm/gems/ruby-1.9.3-p484/bin"
+    PATH="${PATH}:${HOME}/.rvm/gems/ruby-1.9.3-p484@global/bin"
+    PATH="${PATH}:${HOME}/.rvm/rubies/ruby-1.9.3-p484/bin"
+elif [[ -e "${HOME}/.rvm/rubies/ruby-2.2.2" ]]; then
+    PATH="${PATH}:${HOME}/.rvm/gems/ruby-2.2.2/bin"
+    PATH="${PATH}:${HOME}/.rvm/gems/ruby-2.2.2@global/bin"
+    PATH="${PATH}:${HOME}/.rvm/rubies/ruby-2.2.2/bin"
+fi
 
 # Add RVM to PATH for scripting
 PATH="${PATH}:${HOME}/.rvm/bin"
