c7ddcf24c91688f18a726656cbc325019a5a0c2a
Milos Levacic <milos@levacic.net>
2015-08-23 07:30:55 +0200

    Add checkwinsize to bash customizations

--- .bash-customizations/shell.bash
+++ .bash-customizations/shell.bash
@@ -1,2 +1,6 @@
 # Enables expanding "**" in globs
 shopt -s globstar
+
+# Check the window size after each command and, if necessary, update the values
+# of LINES and COLUMNS.
+shopt -s checkwinsize
