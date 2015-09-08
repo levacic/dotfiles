3322ca24f05d2c44c3e1a321714ed84ba2b5346d
Milos Levacic <milos@levacic.net>
2015-09-08 15:04:40 +0200

    Run local gulp binary if available

--- .oh-my-zsh-customizations/gulp.zsh
+++ .oh-my-zsh-customizations/gulp.zsh
@@ -1,2 +1,12 @@
 # Gulp ZSH auto-completion
 eval "$(gulp --completion=zsh)"
+
+# Run locally-installed gulp if available, otherwise try the globally installed
+# version.
+gulp() {
+    if [[ -e "./node_modules/.bin/gulp" ]]; then
+        ./node_modules/.bin/gulp "$@"
+    else
+        command gulp "$@"
+    fi
+}
