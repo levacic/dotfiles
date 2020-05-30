0454504e9cd71653d9f5fb25ef72c1b6df138104
Milos Levacic <milos@levacic.net>
2020-05-30 17:03:14 +0200

    No Bower no more

--- .oh-my-zsh-customizations/bower.zsh
+++
@@ -1,9 +0,0 @@
-# Run locally-installed bower if available, otherwise try the globally installed
-# version.
-bower() {
-    if [[ -e "./node_modules/.bin/bower" ]]; then
-        ./node_modules/.bin/bower "$@"
-    else
-        command bower "$@"
-    fi
-}
