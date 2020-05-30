f3f42bbfc9fd45056e4f6e2dff64bbdec86e86e0
Milos Levacic <milos@levacic.net>
2020-05-30 17:04:09 +0200

    No RVM no more
    This is becoming a theme now.

--- .bash-customizations/rvm.bash
+++
@@ -1,9 +0,0 @@
-# Load RVM into a shell session *as a function*
-if [[ -s "${HOME}/.rvm/scripts/rvm" ]] ; then
-    source "${HOME}/.rvm/scripts/rvm"
-fi
-
-# Add RVM to PATH for scripting
-PATH="${PATH}:${HOME}/.rvm/bin"
-
-export PATH
