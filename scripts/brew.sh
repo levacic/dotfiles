78df8790bce0177c345dd0259c0189f6b964407d
Milos Levacic <milos@levacic.net>
2020-11-17 14:08:53 +0100

    Configure Homebrew

---
+++ scripts/brew.sh
@@ -0,0 +1,7 @@
+# Set up Homebrew environment configuration and paths.
+export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar"
+export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew"
+export PATH="${PATH:+$PATH:}/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin"
+export MANPATH="${MANPATH:+$MANPATH:}/home/linuxbrew/.linuxbrew/share/man"
+export INFOPATH="${INFOPATH:+$INFOPATH:}/home/linuxbrew/.linuxbrew/share/info"
