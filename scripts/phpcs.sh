585e04ca4659256c61125df084c2f861ed21621d
Milos Levacic <milos@levacic.net>
2021-01-20 09:02:21 +0100

    Remove scripts for running Gulp, PHPCS, and PHPUnit
    The scripts were meant to enable automatically running either the
    locally-installed binary within a project, or fallback to a globally-installed
    binary otherwise. However, these tools are nowadays almost exclusively executed
    within Docker containers, so it makes little sense to use these scripts.
    
    If one of them needs to be run on the host machine, it's easy enough to do so
    manually.

--- scripts/phpcs.sh
+++
@@ -1,9 +0,0 @@
-# Run locally-installed PHPCS if available, otherwise try the globally installed
-# version.
-phpcs() {
-    if [[ -e "./vendor/bin/phpcs" ]]; then
-        php ./vendor/bin/phpcs "$@"
-    else
-        command phpcs "$@"
-    fi
-}
--- scripts/phpunit.sh
+++
@@ -1,9 +0,0 @@
-# Run locally-installed PHPUnit if available, otherwise try the globally
-# installed version.
-phpunit() {
-    if [[ -e "./vendor/bin/phpunit" ]]; then
-        php ./vendor/bin/phpunit "$@"
-    else
-        command phpunit "$@"
-    fi
-}
