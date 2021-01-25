a56aea299a2f39b2591f3ec4dbcba83b4471b82e
Milos Levacic <milos@levacic.net>
2021-01-25 22:10:07 +0100

    Alias `ll` to `gls` (via `coreutils`) on macOS

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,2 +1,8 @@
-alias ll='ls -lv --almost-all --human-readable --color --group-directories-first'
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    LS="ls"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    LS="gls"
+fi
+
+alias ll="${LS} -lv --almost-all --human-readable --color --group-directories-first"
 alias gam='~/bin/gam/gam'
