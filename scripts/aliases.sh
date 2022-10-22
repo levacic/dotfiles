7a96b5bd4d79a02baca1c1660fd670a184c8d498
Milos Levacic <milos@levacic.net>
2022-10-22 15:21:18 +0200

    Add `gitkl` alias

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -6,3 +6,6 @@ fi
 
 alias ll="${LS} -lv --almost-all --human-readable --color --group-directories-first"
 alias gam='~/bin/gam/gam'
+# Show local branches in gitk.
+alias gitkl="gitk --argscmd='git for-each-ref --format=\"%(refname)\" refs/heads' &!"
+
