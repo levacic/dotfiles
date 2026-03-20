20cdd04517cffe99aae7eda3d6e8a560e9f5da9f
Milos Levacic <milos@levacic.net>
2026-03-20 14:04:58 +0100

    Abandon attempt at `stderr` pre-processing
    It was buggy in multiple ways, most notably, it didn't play well with programs
    that overwrite output (e.g. various "progress bars" and the like), and it
    completely broke interactive commands - i.e. those that require interactive
    input.
    
    Additionally, it turns out that _so many_ programs output various stuff to
    `stderr` that aren't actually errors, but just additional information that might
    not belong to `stdout` - so this was less useful than anticipated.

--- scripts/stderr.zsh
+++
@@ -1,5 +0,0 @@
-# Prefix error output with a warning sign and a red `[stderr]` tag.
-if [[ -z "$_STDERR_PREFIXED" ]]; then
-  export _STDERR_PREFIXED=1
-  exec 2> >(while IFS= read -r line; do print -Pn "%F{red}⚠️ [stderr]%f " >&2; printf '%s\n' "$line" >&2; done)
-fi
